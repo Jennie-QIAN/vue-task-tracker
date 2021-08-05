1d13d1852ee2ad1b1a3193986ee184e1730ebb18
Zhenni Qian <zhenni.qian913@gmail.com>
2021-08-05 20:06:46 +0200

    kick-off my first vue project

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  name: 'Header',
+  // props: ['title'], => declare props in an array
+  props: {
+    title: String,
+    // title: {
+    //   type: String,
+    //   default: 'Hello Vue.js'
+    // }
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
