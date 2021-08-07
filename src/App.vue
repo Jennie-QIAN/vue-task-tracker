139e75212c54de5e4c8d647e6bec8aa81ea73abc
Zhenni Qian <zhenni.qian913@gmail.com>
2021-08-07 17:27:51 +0200

    feat: implement ui functionalities including adding tasks, toggle add-task, delete task and toggle-reminder

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,68 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <div v-if="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
+
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask(id) {
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter(task => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task)
+    }
+  },
+  created() { //this is a life-cycle method
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Grocery shopping',
+        day: 'August 7th at 11:00am',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Pedicure',
+        day: 'August 7th at 12:00pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Vue conference',
+        day: 'August 8th at 3:00pm',
+        reminder: false,
+      }
+    ]
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button />
+    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'" />
   </header>
 </template>
 
@@ -17,6 +17,7 @@ export default {
     //   type: String,
     //   default: 'Hello Vue.js'
     // }
+    showAddTask: Boolean,
   },
   components: {
     Button
